be6f8715ab76622c8974cdb56ace67d0d1f9d301
Uriah <uriahnaomi.sacdalan@vtsd.com>
2022-09-15 11:58:02 -0400

    My lab is completed.

--- Lab02vst.java
+++ Lab02vst.java
@@ -1,6 +1,27 @@
+//Lab02vst.java
+//
+//
+//
+
+
+
 public class Lab02vst {
 
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
+        System.out.println("Lab 02, 80 Point version");
+        System.out.println();
+        System.out.println();
+        System.out.print("Measure today's knowledge");
+        System.out.println(" by yesterday's confusion.");
+        System.out.println();
+        System.out.println("Bewilderment + Exposure = Obvious");
+        System.out.println();
+        System.out.println("# ##### #####      ##### ##### # #     # ##### #####");
+        System.out.println("# #   # #   #      #   # #   # # # #   #   #   #");
+        System.out.println("# #   # #   #      ##### #   # # #  #  #   #   #####");
+        System.out.println("# #   # #   #      #     #   # # #   # #   #       #");
+        System.out.println("# ##### #####      #     ##### # #     #   #   #####");
 
 
     }
